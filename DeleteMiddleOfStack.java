ba5043175155110eaa51c473dc856ef8f899d353
Nitin Jayant <thenitinjayant@gmail.com>
2020-11-20 17:54:43 +0530

    delete middle of stack

---
+++ DeleteMiddleOfStack.java
@@ -0,0 +1,14 @@
+package GFG_Questions;
+
+import java.util.Stack;
+
+public class DeleteMiddleOfStack {
+
+    public int deleteMiddle(Stack<Integer> stack){
+
+    }
+
+    public void mainDeleteMiddleOfStack(){
+
+    }
+}
