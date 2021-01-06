de513ab67de6fe7b737dc5d7db378da0602e463d
Nitin Jayant <thenitinjayant@gmail.com>
2021-01-06 15:04:01 +0530

    runner added

---
+++ GFGRunner.java
@@ -0,0 +1,10 @@
+package GFG_Questions;
+
+public class GFGRunner {
+    public void run(){
+        DeleteMiddleOfStack deleteMiddleOfStack = new  DeleteMiddleOfStack();
+
+        deleteMiddleOfStack.mainDeleteMiddleOfStack();
+    }
+}
+
