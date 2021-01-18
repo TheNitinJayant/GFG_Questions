3db0dcf52623452ddc5beb60d51b94ae4a7f826d
Nitin Jayant <thenitinjayant@gmail.com>
2021-01-18 22:48:21 +0530

    delete middle of stack

--- DeleteMiddleOfStack.java
+++ DeleteMiddleOfStack.java
@@ -4,11 +4,36 @@ import java.util.Stack;
 
 public class DeleteMiddleOfStack {
 
-    public int deleteMiddle(Stack<Integer> stack){
+    public void deleteMiddle(Stack<Integer> stack, int sizeOfStack, int current){
 
+        if(stack.isEmpty() || current == sizeOfStack){
+            return ;
+        }
+
+        int el = stack.pop();
+
+        deleteMiddle(stack, sizeOfStack, current+1);
+
+        if(current != sizeOfStack/2){
+            stack.push(el);
+        }
     }
 
     public void mainDeleteMiddleOfStack(){
 
+        Stack<Integer> stack = new Stack();
+
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+        stack.push(40);
+        stack.push(50);
+
+        System.out.println(stack);
+        System.out.println(stack.size());
+
+        deleteMiddle(stack, stack.size(), 0);
+
+        System.out.println(stack);
     }
 }
